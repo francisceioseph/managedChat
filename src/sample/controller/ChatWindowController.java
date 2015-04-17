95fa18ab1240cfb9ea65cada46038d1285afaad6
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-04-17 09:00:03 -0300

    Termino da implementação da tela de chat

--- src/sample/controller/ChatWindowController.java
+++ src/sample/controller/ChatWindowController.java
@@ -1,26 +1,21 @@
 package sample.controller;
 
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
 import javafx.util.Callback;
-import net.jini.core.entry.UnusableEntryException;
 import net.jini.core.transaction.TransactionException;
 import sample.helper.Singleton;
+import sample.helper.MessagesFinder;
 import sample.model.MessageTuple;
-import sample.model.UserInformationTuple;
-import sun.plugin2.message.Message;
 
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -33,8 +28,8 @@ public class ChatWindowController implements Initializable{
     public TextField messageTextField;
     private String anotherUsername;
 
-    public ListView<Label> chatListView;
-    private ObservableList<Label> messages;
+    public ListView<MessageTuple> chatListView;
+    private ObservableList<MessageTuple> messages;
 
     public void enviarMensagem(ActionEvent actionEvent) {
         MessageTuple tupleToSend = new MessageTuple(
@@ -44,8 +39,9 @@ public class ChatWindowController implements Initializable{
                 false
         );
 
-        messages.add(this.makeChatBubble(messageTextField.getText(), true));
-
+        messages.add(tupleToSend);
+        messageTextField.clear();
+        
         try {
             Singleton.INSTANCE.writeMessageOnSpace(tupleToSend);
         } catch (RemoteException e) {
@@ -55,69 +51,74 @@ public class ChatWindowController implements Initializable{
         }
     }
 
-    public Label makeChatBubble(String message, boolean direction){
-        Label label = new Label(message);
-        label.setFont(new Font(16));
-
-        if (direction) {
-            label.setStyle("-fx-shape:\"m 211.78571,327.00506 265.35715,1.42857 c 3.43583,0.42441 7.17703,0.33987 7.85714,5.35715 l 0.35714,130.35714 c -0.68955,6.94804 -4.85362,7.81568 -9.28571,8.21429 L 242.5,472.00506 c -2.89525,-1.2809 -6.19455,0.26662 -8.21429,-7.14285 l 0.35715,-111.07143 z\";" +
-                            "-fx-background-color: #AD1457;" +
-                            "-fx-background-insets:0,0;" +
-                            "-fx-padding: 50;" +
-                            "-fx-background-size: contain, contain;"
-            );
-        }
-        else{
-            label.setStyle("-fx-shape:\"m 484.76823,327.00506 -265.35715,1.42857 c -3.43583,0.42441 -7.17703,0.33987 -7.85714,5.35715 l -0.35714,130.35714 c 0.68955,6.94804 4.85362,7.81568 9.28571,8.21429 l 233.57143,-0.35715 c 2.89525,-1.2809 6.19455,0.26662 8.21429,-7.14285 l -0.35715,-111.07143 z\";" +
-                    "-fx-background-color: #673AB7;" +
-                    "-fx-background-insets: 0,1;" +
-                    "-fx-padding: 50;");
-        }
-
-        return label;
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         messages = FXCollections.observableArrayList();
 
         chatListView.setItems(messages);
 
-        Thread thread = new Thread(new MessagesFinder());
+        chatListView.setCellFactory(new Callback<ListView<MessageTuple>, ListCell<MessageTuple>>() {
+            @Override
+            public ListCell<MessageTuple> call(ListView<MessageTuple> messageTupleListView) {
+                return new MessagesListCell();
+            }
+        });
+
+        MessageTuple template = new MessageTuple(anotherUsername, Singleton.INSTANCE.username, null, true);
+        Thread thread = new Thread(new MessagesFinder(true, anotherUsername, template));
         thread.start();
     }
 
-    class MessagesFinder implements Runnable {
-        public boolean alive = true;
-        private final MessageTuple template = new MessageTuple(anotherUsername, Singleton.INSTANCE.username, null, true);
+    static class MessagesListCell extends ListCell<MessageTuple>{
+        private final Font labelFont = new Font(12);
 
         @Override
-        public void run() {
-            while (alive) {
-                try {
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
+        public void updateItem(MessageTuple item, boolean empty) {
+            super.updateItem(item, empty);
+            final Label label = new Label();
+            final HBox hBox = new HBox();
 
-                            MessageTuple tuple = null;
-                            try {
-//                                tuple = Singleton.INSTANCE.readMessageFromSpace(template);
-//
-//                                if (tuple != null)
-//                                    messages.add(tuple);
+            if (item != null) {
+                StringBuilder builder = new StringBuilder();
+                int tokensnumber = 0;
 
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
+                for (String token : item.content.split(" ")){
+                    tokensnumber++;
 
-                        }
-                    });
-
-                    Thread.sleep(300);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    if (tokensnumber % 3 == 0){
+                        builder.append(token.concat("\n"));
+                    }
+                    else {
+                        builder.append(token.concat(" "));
+                    }
                 }
+
+                label.setText(builder.toString());
+                label.setFont(labelFont);
+
+                if (item.userFrom.equals(Singleton.INSTANCE.username)) {
+                    label.setStyle("-fx-shape:\"m 263.40043,371.81354 239.20661,0 c 11.29436,2.52786 12.09164,7.15514 13.26758,11.70669 l 0.78044,108.87218 -252.47419,1.17066 c -8.99968,-2.6432 -10.99897,-9.28662 -12.87735,-15.99913 l 0,-88.97081 c 0.89009,-7.29788 2.65743,-14.11985 12.09691,-16.77959 z\";" +
+                                    "-fx-background-color: #AD1457;" +
+                                    "-fx-background-insets:0,0;" +
+                                    "-fx-padding: 10, 10, 10, 20;" +
+                                    "-fx-scale-shape: true;" +
+                                    "-fx-background-size:cover;" +
+                                    "-fx-text-fill: white;"
+                    );
+                }
+                else{
+                    label.setStyle("-fx-shape:\"m 504.56173,371.81354 -239.20661,0 c -11.29436,2.52786 -12.09164,7.15514 -13.26758,11.70669 l -0.78044,108.87218 252.47419,1.17066 c 8.99968,-2.6432 10.99897,-9.28662 12.87735,-15.99913 l 0,-88.97081 c -0.89009,-7.29788 -2.65743,-14.11985 -12.09691,-16.77959 z\";" +
+                                    "-fx-background-color: #673AB7;" +
+                                    "-fx-background-insets:0,0;" +
+                                    "-fx-padding: 10, 10, 10, 20;" +
+                                    "-fx-scale-shape: true;" +
+                                    "-fx-background-size:cover;" +
+                                    "-fx-text-fill: white;"
+                    );
+                }
+                hBox.setAlignment(Pos.CENTER_RIGHT);
+                hBox.getChildren().add(label);
+                setGraphic(hBox);
             }
         }
     }
